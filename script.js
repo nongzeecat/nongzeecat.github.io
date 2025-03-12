445bfb27131fe1d58b85fae0e22e6f5894b27c40
nongzeelovecatmakmak <kornhamdam@gmail.com>
2025-03-12 18:58:06 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("button").addEventListener("click", function () {
+    document.getElementById('font').style.color = document.getElementById('font').style.color === "red" ? "blue" : "red";
+});
